c33afb1dc9c9cd54c37ddbe1235baaeb74fdc827
Nguyen Ngoc Tuan <unlbynnt@gmail.com>
2025-03-26 15:00:01 +0700

    first commit

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,12 @@
+import Header from "./Header.jsx"
+import Footer from "./Footer.jsx"
+function App() {
+  return(
+    <>
+    <Header/>
+    <Footer/>
+    </>
+  );
+}
+
+export default App
---
+++ my-react-app/src/Footer.jsx
@@ -0,0 +1,9 @@
+
+function Footer(){
+    return(
+        <footer>
+            <p>&copy; {new Date().getFullYear()} Copyright</p>
+        </footer>
+    );
+}
+export default Footer
